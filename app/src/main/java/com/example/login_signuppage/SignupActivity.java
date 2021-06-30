2dfb128801dec68266b3170da77ee4310427cc9a
AK <ktmabhi007@gmail.com>
2021-06-30 15:10:11 +0530

    Update

---
+++ app/src/main/java/com/example/login_signuppage/SignupActivity.java
@@ -0,0 +1,30 @@
+package com.example.login_signuppage;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void OpenSigninPage(View view){
+
+        Intent myIntent = new Intent(SignupActivity.this,MainActivity.class);
+        startActivity(myIntent);
+    }
+}
